65d2609b25e25ec9572ef20b8439a18641926daf
Nardos276 <nardiassefa97@gmail.com>
2025-11-29 11:11:29 +0300

    first commit

---
+++ intro.py
@@ -0,0 +1,8 @@
+#name= input("Name: ")
+#print ("Hello, " + name)
+#print (f"Hello, {name}")
+"""name = 'nardos'
+print (name)
+print ("He's name is 'Johnny'")"""
+a = "Lorem ipsum dolor sit amet \n consectetur adipiscing elit \n sed do eiusmod tempor incididunt \n ut labore et dolore magna aliqua."
+print(a)
