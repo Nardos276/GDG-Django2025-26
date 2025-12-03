bf722e0c4d43a56c32b0fe0cbd66680a6ea96536
Nardos276 <nardiassefa97@gmail.com>
2025-12-03 15:06:10 +0300

    Working on the exercises

--- intro.py
+++
@@ -1,8 +0,0 @@
-#name= input("Name: ")
-#print ("Hello, " + name)
-#print (f"Hello, {name}")
-"""name = 'nardos'
-print (name)
-print ("He's name is 'Johnny'")"""
-a = "Lorem ipsum dolor sit amet \n consectetur adipiscing elit \n sed do eiusmod tempor incididunt \n ut labore et dolore magna aliqua."
-print(a)
