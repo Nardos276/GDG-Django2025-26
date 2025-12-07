a0278e9615570580ac799b22af6a5f6f9ee31388
Nardos276 <nardiassefa97@gmail.com>
2025-12-07 22:11:42 +0300

    Moving the zeros to the end

---
+++ move_zeros.py
@@ -0,0 +1,13 @@
+nums=[]
+n=int(input("How many numbers do you want in the array:" ))
+for i in range(n):
+    nums.append(int(input("Enter the numbers: ")))
+for j in range (len(nums)):
+    for i in range(len(nums)-1):
+        if nums[i]==0:
+            temp=nums[i]
+            nums[i]=nums[i+1]
+            nums[i+1]=temp
+        else:
+            continue
+print("Output: ", nums)
